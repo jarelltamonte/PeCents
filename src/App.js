dac33bda20b974741a1ce2dd1974dc71cdf1640d
CIITSHSDTP00\CIIT <anamggss1227@gmail.com>
2025-11-20 14:39:11 +0800

    Home Page Progress

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Navbar from './Navbar';
-import SavingsPage from './SavingsPage';
+import "./App.css";
+import Navbar from "./Navbar";
+import SavingsPage from "./SavingsPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-          <Routes>
-            <Route path="/savings" element={<SavingsPage />} />
-          </Routes>
+        <Routes>
+          <Route path="/savings" element={<SavingsPage />} />
+        </Routes>
       </Router>
       <main>
         <div className="Dashboard">
-          <h1>UwU</h1>
+          <h1>Hello mga pare, gawin ko to paguwi. Chill muna sa page na ito</h1>
+          <br></br>
 
-          <div className='Dashboard-container'>
-
+          <div className="Dashboard-container">
             {/* Column 1 */}
             <div className="Dashboard-column">
               <div className="Funds">
-                <h1 className='left'> Welcome, Spongebob</h1>
-                <h1 className='right'>$100</h1>
+                <h1 className="Funds-name">Welcome, Spongebob</h1>
+                <h1 className="Funds-value">$100</h1>
+              </div>
+
+              <div className="Dashbuttons">
+                <button style={{ flex: "1" }}>MTS</button>
+                <button>Deposit</button>
+                <button>Withdraw</button>
+              </div>
+
+              <div>
+                <h1>Savings Plans Display</h1>
               </div>
             </div>
 
             {/* Column 2 */}
             <div className="Dashboard-column">
-              <h1> Hi </h1>
+              <h1>GRAPH</h1>
+              <div className="C2-Description">
+                <h4> Total Funds Deposited: $0 💀</h4>
+                <h4> Total Funds Withdrawn: $1,000,000 </h4>
+              </div>
             </div>
-
           </div>
         </div>
       </main>
