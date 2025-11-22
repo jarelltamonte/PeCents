487d46ff865dc952fae4d0badc37dbdc25baf7e8
AnamXI <134472175+AnamXI@users.noreply.github.com>
2025-11-22 14:02:17 +0800

    Update to Dashboard Interface

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,21 +7,21 @@ function HomePage() {
         <div className="App">
             <main>
                 <div className="Dashboard">
-                    <h1>Hello mga pare, gawin ko to paguwi. Chill muna sa page na ito</h1>
+                    <h1 style={{ fontSize: "50px" }}> Pecents Monsense</h1>
                     <br></br>
 
                     <div className="Dashboard-container">
                         {/* Column 1 */}
                         <div className="Dashboard-column">
                             <div className="Funds">
-                                <h1 className="Funds-name">Welcome, Spongebob</h1>
-                                <h1 className="Funds-value">$100</h1>
+                                <h2 className="Funds-name">Welcome, Spongebob</h2>
+                                <h2 className="Funds-value">$100</h2>
                             </div>
 
                             <div className="Dashbuttons">
                                 <button style={{ flex: "1" }}>MTS</button>
-                                <button>Deposit</button>
-                                <button>Withdraw</button>
+                                <button>DEPOSIT</button>
+                                <button>WITHDRAW</button>
                             </div>
 
                             <div>
@@ -33,8 +33,8 @@ function HomePage() {
                         <div className="Dashboard-column">
                             <h1>GRAPH</h1>
                             <div className="C2-Description">
-                                <h4> Total Funds Deposited: $0 💀</h4>
-                                <h4> Total Funds Withdrawn: $1,000,000 </h4>
+                                <h3> Total Funds Deposited: $0 💀</h3>
+                                <h3> Total Funds Withdrawn: $1,000,000 </h3>
                             </div>
                         </div>
                     </div>
